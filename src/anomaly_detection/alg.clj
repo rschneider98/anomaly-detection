ddf3690ae43c0ed315e469fdc0d976d3c23819b2
Richard <rschneider1st@gmail.com>
2021-04-26 14:26:15 -0500

    added basic outline of functions

--- src/anomaly_detection/alg.clj
+++ src/anomaly_detection/alg.clj
@@ -52,3 +52,52 @@
            [(deref new_graph) (deref new_changes)]
            :else
            (recur new_graph new_queue new_changes))))
+
+
+(defn count-min-dampen 
+  "Reduce the counts in the count-min sketch data structure. Used for weighted averages of struct
+   (count-min-dampen count-min) will return modified count-min with factor=0.85
+   (count-min-dampen count-min factor) will return modified count-min"
+  ;; Count-min stores counters in vector of a map, modify this vector and remake map
+  [cm]
+  (count-min-dampen cm 0.85)
+  [cm factor]
+  (assoc cm :counters (map (fn [x] (* factor x)) (:counters cm))))
+
+
+(defn count-edge
+  "Given count-min structure and edge map, add into the struct and queue of 
+   time-based seen objects
+   (count-edge count-min edge-map)"
+)
+
+
+(defn count-node
+  "Given a count-min structure and node id, add into the struct and queue of
+   time-based seen objects
+   (count-node count-min node-id)"
+)
+
+
+(defn add-edge
+  "Given an edge map, add the edge to the database
+   (add-edge edge-map)"
+)
+
+
+(defn add-node
+  "Given a node and properties, add the node to the database
+   (add-node node-id properties)"
+)
+
+
+(defn concern?
+  "Given a count-min structure, value to check, and threshold, see if it raises a concern
+   (concern? count-min value threshold)"
+)
+
+
+(defn report 
+  "Output concerns to a file, takes collection of maps and filename, saves as CSV
+   (report concerns filename)"
+)
